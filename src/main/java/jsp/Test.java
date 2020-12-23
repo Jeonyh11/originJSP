d1a01f567b8ac7203187b59d703497bf41da4fd0
JYH <immense3@gmail.com>
2020-12-23 10:18:33 +0900

    1.jsp 프로젝트 환경 구성 2. 20201222일 수업내용 정리

---
+++ src/main/java/jsp/Test.java
@@ -0,0 +1,45 @@
+package jsp;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Test.java
+ *
+ * @author PC11-PC
+ * @version 1.0
+ * @see
+ *
+ * <pre>
+ * << 개정이력(Modification Information) >>
+ *
+ * 수정자 수정내용
+ * ------ ------------------------
+ * PC11-PC 최초 생성
+ *
+ * </pre>
+ */
+public class Test {
+
+	
+	/**
+	* Method : main
+	* 작성자 : PC11-PC
+	* 변경이력 :
+	* 
+	* 
+	* 
+	* 
+	* @param args
+	* Method 설명 :
+	*/
+	public static void main(String[] args) {
+		/***Given***/
+		
+
+		/***When***/
+
+		/***Then***/
+		private static final Logger logger = LoggerFactory.getLogger(Test.class);
+	}
+}
